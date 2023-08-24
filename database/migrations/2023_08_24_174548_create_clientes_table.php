b3f2a2fa65d10f0369c0118c9b193f3cef9a96ea
Mateo-Bazan <mateobazansanabria@gmail.com>
2023-08-24 15:08:56 -0300

    Base de datos migrada a laravel

---
+++ database/migrations/2023_08_24_174548_create_clientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id("id_cliente");
+            $table->string("dni");
+            $table->string("nombre_apellido");
+            $table->string("nro_telefono");
+            $table->string("email");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
